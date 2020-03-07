5c00f4cf8872f2313640dfa6889362fd80ce597b
Petrichor <54273332+petrichor@users.noreply.github.com>
2020-03-07 15:06:59 +0800

    Change github pages publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/HexPass' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/generator' : '/',
   pwa: {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
